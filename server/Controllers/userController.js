481bd0b844fc025ca6795ce85fa3dcf105f8ecf9
Vache95 <vachevirabyan1995@gmail.com>
2024-04-11 16:15:01 +0400

    add routes register login and finde user users

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,7 +1,98 @@
 const userModel = require("../Modules/userModel")
+const bcrypt = require("bcrypt")
+const validator = require("validator")
+const jwt = require("jsonwebtoken")
 
-const registerUser = (req, res) => {
-    res.send("Register")
+const createToken = (_id) => {
+
+    const jwtKey = process.env.JWT_SECRET_KEY
+
+    return jwt.sign({ _id }, jwtKey, { expiresIn: "3d" });
+
 }
 
-module.exports = {registerUser}
+const registerUser = async (req, res) => {
+    try {
+
+        const { name, email, password } = req.body
+
+        let user = await userModel.findOne({ email })
+
+        if (user) return res.status(400).json("User with the given email already exist...")
+
+        if (!name || !email || !password) return res.status(400).json("All fields are required")
+
+        if (!validator.isEmail(email)) return res.status(400).json("Email must be a valid email...")
+
+        if (!validator.isStrongPassword(password)) return res.status(400).json("Passowrd must be a strong a passowrd...")
+
+        user = new userModel({ name, email, passowrd })
+
+        const salt = await bcrypt.genSalt(10)
+
+        user.password = await bcrypt.hash(user.password, salt)
+
+        await user.save()
+
+        const token = createToken(user._id)
+
+        res.status(200).json({ _id: user._id, name, email, token })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error);
+    }
+
+}
+
+const loginUser = async (req, res) => {
+    const { email, passowrd } = req.body;
+
+    try {
+
+        let user = await userModel.findOne({ email })
+
+        if (!user) return res.status(400).json("Invalid email or password...")
+
+        const isValidPassowrd = await bcrypt.compare(passowrd, user.password)
+
+        if (!isValidPassowrd) return res.status(400).json("Invalid email or password...")
+
+        const token = createToken(user._id)
+
+        res.status(200).json({ _id: user._id, name: user.name, email, token })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error);
+    }
+}
+
+
+const findUser = async (req, res) => {
+    const userId = req.params.userId
+    try {
+
+        let user = await userModel.findById(userId)
+
+        res.status(200).json(user)
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error);
+    }
+}
+
+const getUser = async (req, res) => {
+    try {
+
+        let users = await userModel.find()
+
+        res.status(200).json(users)
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error);
+    }
+}
+module.exports = { registerUser, loginUser, findUser,getUser }
